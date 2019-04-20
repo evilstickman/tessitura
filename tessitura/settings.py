85e525f03d1f1c83fa5ff4117a98a0e37d96beea
Matt Billock <matt.billock@gmail.com>
2019-04-20 00:00:36 -0500

    Moved include of django heroku

--- tessitura/settings.py
+++ tessitura/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -120,5 +121,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
