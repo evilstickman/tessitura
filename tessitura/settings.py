44be965c67d174e5637c0db138a6db22b9b5de6a
Matt Billock <mattbillock@mattbillock.com>
2019-05-17 15:58:04 -0500

    Added root menu template

--- tessitura/settings.py
+++ tessitura/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tessitura.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['perform/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
