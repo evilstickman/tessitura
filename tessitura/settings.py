270223f46d038779516db69e66cc45ad3c4a2c95
Matt Billock <mattbillock@mattbillock.com>
2019-05-17 23:31:56 -0500

    Added initial models and mgirations

--- tessitura/settings.py
+++ tessitura/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'perform',
+    'publish',
+    'coordinate',
+    'direct',
 ]
 
 MIDDLEWARE = [
